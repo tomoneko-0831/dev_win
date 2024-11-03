93909b960b698e27c48d87f463b1cc318b53d60b
TOMONEKO <tomoneko.inc@gmail.com>
2024-11-03 14:33:00 +0900

    improve env

---
+++ settings/32bit-set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CC=i686-w64-mingw32-gcc
+export CXX=i686-w64-mingw32-c++
+export LD=i686-w64-mingw32-ld
+export AR=i686-w64-mingw32-ar
+export AS=i686-w64-mingw32-as
+export NM=i686-w64-mingw32-nm
+export STRIP=i686-w64-mingw32-strip
+export RANLIB=i686-w64-mingw32-ranlib
+export DLLTOOL=i686-w64-mingw32-dlltool
+export OBJDUMP=i686-w64-mingw32-objdump
+export RESCOMP=i686-w64-mingw32-windres
+export WINDRES=i686-w64-mingw32-windres
---
+++ settings/64bit-set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CC=x86_64-w64-mingw32-gcc
+export CXX=x86_64-w64-mingw32-c++
+export LD=x86_64-w64-mingw32-ld
+export AR=x86_64-w64-mingw32-ar
+export AS=x86_64-w64-mingw32-as
+export NM=x86_64-w64-mingw32-nm
+export STRIP=x86_64-w64-mingw32-strip
+export RANLIB=x86_64-w64-mingw32-ranlib
+export DLLTOOL=x86_64-w64-mingw32-dlltool
+export OBJDUMP=x86_64-w64-mingw32-objdump
+export RESCOMP=x86_64-w64-mingw32-windres
+export WINDRES=x86_64-w64-mingw32-windres
